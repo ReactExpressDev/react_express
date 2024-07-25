6611743a32d8aabff0884a8481bcd8a42c931508
endo <tky060313@outlook.jp>
2024-07-25 23:36:20 +0900

    ssl設定変更

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-        rejectUnauthorized: true
+        rejectUnauthorized: false
     }
 });
 
