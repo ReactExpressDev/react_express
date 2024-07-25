b98b71d2bfd1d8aad34989299459108841d52985
endo <tky060313@outlook.jp>
2024-07-25 16:39:31 +0900

    index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM your_table');
+        res.json(result.rows);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+});
 
 app.get('/api', (req, res) => {
     res.send('Hello from Express!');
